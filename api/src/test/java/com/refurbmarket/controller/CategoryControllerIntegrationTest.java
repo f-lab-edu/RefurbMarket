8779c7d0ba898cc5a41563030418577868231a40
mingmong <mingmong@gmail.com>
2024-10-30 18:53:02 +0900

    refactor: 개별 클래스들이 다 포함되도록 import 설정 수정

--- api/src/test/java/com/refurbmarket/controller/CategoryControllerIntegrationTest.java
+++ api/src/test/java/com/refurbmarket/controller/CategoryControllerIntegrationTest.java
@@ -1,6 +1,6 @@
 package com.refurbmarket.controller;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
--- api/src/test/java/com/refurbmarket/controller/UserControllerIntegrationTest.java
+++ api/src/test/java/com/refurbmarket/controller/UserControllerIntegrationTest.java
@@ -1,6 +1,6 @@
 package com.refurbmarket.controller;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
